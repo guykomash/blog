82705c787cd9c4d76735e79691b2fb1c8eb82e36
guykomash <gkomash@gmail.com>
2026-02-19 00:38:56 +0200

    /blog routing fixed & lint

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://guykomash.github.io",
-    base: "/blog"
+  site: "https://guykomash.github.io",
+  base: "/blog",
 });
